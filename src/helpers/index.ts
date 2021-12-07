c191e716469dc02ca4c00d48185de27bbda0cd65
PhuSang <sang.le@ninecode.vn>
2021-12-07 18:04:00 +0700

    parse url

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -13,70 +13,26 @@ export default function cleanQueryParams(query:any) {
  * @returns {object} 
  */
 
-   
- const  getAllUrlParams: any = function (url:string):object {
-    // get query string from url (optional) or window
-    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-
-    // we'll store the parameters here
-    var obj:any = {};
-
-    // if query string exists
-    if (queryString) {
-
-        // stuff after # is not part of query string, so get rid of it
-        queryString = queryString.split('#')[0];
-
-        // split our query string into its component parts
-        var arr = queryString.split('&');
-
-        for (var i = 0; i < arr.length; i++) {
-            // separate the keys and the values
-            var a = arr[i].split('=');
-
-            // set parameter name and value (use 'true' if empty)
-            var paramName = a[0];
-            var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-
-            // (optional) keep case consistent
-            paramName = paramName.toLowerCase();
-            if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-
-            // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-            if (paramName.match(/\[(\d+)?\]$/)) {
-
-                // create key if it doesn't exist
-                var key = paramName.replace(/\[(\d+)?\]/, '');
-                if (!obj[key]) obj[key] = [];
-
-                // if it's an indexed array e.g. colors[2]
-                if (paramName.match(/\[\d+\]$/)) {
-                    // get the index value and add the entry at the appropriate position
-                    var value:any = /\[(\d+)\]/.exec(paramName);
-                    var index=value[1];
-                    obj[key][index] = paramValue;
-                } else {
-                    // otherwise add the value to the end of the array
-                    obj[key].push(paramValue);
-                }
-            } else {
-                // we're dealing with a string
-                if (!obj[paramName]) {
-                    // if it doesn't exist, create property
-                    obj[paramName] = paramValue;
-                } else if (obj[paramName] && typeof obj[paramName] === 'string'){
-                    // if property does exist and it's a string, convert it to an array
-                    obj[paramName] = [obj[paramName]];
-                    obj[paramName].push(paramValue);
-                } else {
-                    // otherwise add the property
-                    obj[paramName].push(paramValue);
-                }
+ function parseURL(url:string) {
+    var a =  document.createElement('a');
+    a.href = url;
+    return {
+        host: a.hostname,
+        port: a.port,
+        query: a.search,
+        params: (function(){
+            var ret:any = {},
+                seg = a.search.replace(/^\?/,'').split('&'),
+                len = seg.length, i = 0, s;
+            for (;i<len;i++) {
+                if (!seg[i]) { continue; }
+                s = seg[i].split('=');
+                ret[s[0]] = s[1];
             }
-        }
-    }
-
-    return obj;
+            return ret;
+        })(),
+        hash: a.hash.replace('#','')
+    };
 }
 
 
@@ -85,4 +41,4 @@ const generatorMediaURL=(id:any,urlDefault="")=>{
 }
 
   
-export {getAllUrlParams,cleanQueryParams,generatorMediaURL}
+export { parseURL, cleanQueryParams, generatorMediaURL };
